6dc20d6692cdbeb7ae1d0f489485866537707d1e
Maciej Ruciński <maciej.rucinski@paramount.com>
2023-12-04 15:30:09 +0100

    first-commit-remains

---
+++ src/main/java/tech/paramount/rucinskimaciej/aoc2023/Aoc2023Application.java
@@ -0,0 +1,13 @@
+package tech.paramount.rucinskimaciej.aoc2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aoc2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aoc2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/tech/paramount/rucinskimaciej/aoc2023/Aoc2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.paramount.rucinskimaciej.aoc2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aoc2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
