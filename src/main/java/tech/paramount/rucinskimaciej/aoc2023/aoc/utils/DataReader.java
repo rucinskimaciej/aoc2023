89a4f36f0b558d912aa462d8a274e4c0a6450142
Maciej Ruciński <maciej.rucinski@paramount.com>
2023-12-05 01:13:04 +0100

    implement DataReader

---
+++ src/main/java/tech/paramount/rucinskimaciej/aoc2023/aoc/utils/DataReader.java
@@ -0,0 +1,39 @@
+package tech.paramount.rucinskimaciej.aoc2023.aoc.utils;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+
+
+public class DataReader {
+
+    public List<String> read(Class<?> clazz, DataType dataType) {
+        for (String subDir : clazz.getPackageName().split("\\.")) {
+            if (subDir.startsWith("day")) {
+               return read("aoc/%s/%s".formatted(subDir, dataType.fileName));
+            }
+        }
+        throw new RuntimeException("Well, fuck... we've got nothing!");
+    }
+
+    private List<String> read(String resource) {
+        Optional<URL> data = Optional.ofNullable(this.getClass().getClassLoader().getResource(resource));
+        try {
+            return Files.readAllLines(Path.of(data.orElseThrow().getPath()));
+        } catch (IOException e) {
+            throw new RuntimeException("Well, fuck...", e);
+        }
+    }
+
+
+    @RequiredArgsConstructor
+        public enum DataType {
+            SAMPLE("sample"), DATA("data");
+
+            private final String fileName;
+        }
+}
